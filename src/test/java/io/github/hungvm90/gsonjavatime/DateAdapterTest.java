13918fb9127ef34fd1ebbb2d36072d4ba08b5201
Hung Vu <hungvm.ict@gmail.com>
2024-01-04 23:19:40 +0700

    Add testcase

--- src/test/java/io/github/hungvm90/gsonjavatime/DateAdapterTest.java
+++ src/test/java/io/github/hungvm90/gsonjavatime/DateAdapterTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +30,25 @@ public class DateAdapterTest {
         Date date = new Date(2023 - 1900, Calendar.NOVEMBER, 16, 22, 13, 15);
         Date getBack = gson.fromJson(oldGson.toJson(date), Date.class);
         assertEquals(date.getTime() / 1000, getBack.getTime() / 1000);
+    }
+
+    @Test
+    public void testThreadSafe() throws Exception {
+        Gson oldGson = new Gson();
+        Gson gson = JavaTimeConverters.registerDate(new GsonBuilder()).create();
+        Date date = new Date(2023 - 1900, Calendar.NOVEMBER, 16, 22, 13, 15);
+        String data = oldGson.toJson(date);
+        AtomicInteger count = new AtomicInteger(0);
+        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(() -> {
+                Date getBack = gson.fromJson(data, Date.class);
+                assertEquals(date.getTime() / 1000, getBack.getTime() / 1000);
+                count.incrementAndGet();
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+        assertEquals(1000, count.get());
     }
 }
