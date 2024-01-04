bd30d82268876722401a9bbbc489f5f16e70edb5
Hung Vu <hungvm.ict@gmail.com>
2024-01-04 22:48:11 +0700

    Fix thread safe DateAdapter

--- src/main/java/io/github/hungvm90/gsonjavatime/DateAdapter.java
+++ src/main/java/io/github/hungvm90/gsonjavatime/DateAdapter.java
@@ -4,18 +4,13 @@ import com.fatboyindustrial.gsonjavatime.InstantConverter;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class DateAdapter implements JsonSerializer<Date>, JsonDeserializer<Date> {
 
     private final InstantConverter converter = new InstantConverter();
-    private final DateFormat oldDateFormat;
 
     public DateAdapter() {
-        oldDateFormat = new SimpleDateFormat("MMM dd, yyyy, K:mm:ss a", Locale.US);
     }
 
     @Override
@@ -29,7 +24,7 @@ public class DateAdapter implements JsonSerializer<Date>, JsonDeserializer<Date>
 
     private Date parse(String v) {
         try {
-            return oldDateFormat.parse(v);
+            return new Date(v);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
